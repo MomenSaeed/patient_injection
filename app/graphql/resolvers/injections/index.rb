9202425ec78840826330eb0986ddf32d84c1b80b
Momen Yacoub <momenfci@gmail.com>
2023-02-28 22:12:27 +0100

    adding injections wit patient filter

--- app/graphql/resolvers/injections/index.rb
+++ app/graphql/resolvers/injections/index.rb
@@ -1,8 +1,10 @@
 class Resolvers::Injections::Index < Resolvers::BaseResolver
   type Types::Injection::Type.connection_type, null: true
 
-  def resolve
-    ::Injections::Index.call.injections
+  argument :patient_id, ID, required: true
+
+  def resolve(patient_id:)
+    ::Injections::Index.call(patient_id:).injections
   end
 end
 
--- app/interactors/injections/index.rb
+++ app/interactors/injections/index.rb
@@ -2,8 +2,16 @@ class Injections::Index < BaseInteractor
   ERROR_MESSAGE = "injection.list_process_fails".freeze
   delegate :injections, to: :context
 
-  def call
+  before :set_injections, :with_patient
+
+  def call; end
+
+  def set_injections
     context.injections = Injection.all
+  end
+
+  def with_patient
+    context.injections = injections.where(patient_id: context.patient_id) if context.patient_id.present?
   end
 end
 
