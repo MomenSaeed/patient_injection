68bd9921d9238bf3e2b06a1f1ffb3fe2d279d512
Momen Yacoub <momenfci@gmail.com>
2023-03-01 11:05:46 +0100

    remoe node type description

--- app/graphql/types/node_type.rb
+++ app/graphql/types/node_type.rb
@@ -1,6 +1,4 @@
 module Types::NodeType
-  description "Node Type as parent for single models type"
-
   include Types::BaseInterface
   # Add the `id` field
   include GraphQL::Types::Relay::NodeBehaviors
