8aea8950fda86138cb9236407d954b42cab70895
Momen Yacoub <momenfci@gmail.com>
2023-03-01 18:28:12 +0100

    fix simple coverage issue

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,11 +1,4 @@
 require "simplecov"
-require "simplecov_json_formatter"
-
-SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
-                                                                  SimpleCov::Formatter::HTMLFormatter,
-                                                                  SimpleCov::Formatter::JSONFormatter,
-                                                                ])
-SimpleCov.minimum_coverage 70.0
 
 SimpleCov.start "rails"
 
