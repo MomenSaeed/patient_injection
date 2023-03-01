18102175cd38692be29d19df8943c704863ffce6
Momen Yacoub <momenfci@gmail.com>
2023-03-01 09:05:06 +0100

    adding created at field to patient type

--- app/graphql/types/patient_type.rb
+++ app/graphql/types/patient_type.rb
@@ -4,6 +4,7 @@ class Types::PatientType < Types::BaseObject
   description "Patient Type"
 
   field :api_key, String, null: false, description: "apiKey field => used for Authentication"
+  field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :full_name, String, null: false, description: "full_name field"
   field :id, ID, null: false, description: "id field"
 end
